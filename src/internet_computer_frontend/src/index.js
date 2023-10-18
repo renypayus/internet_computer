6e108cf6be51e5968e769be7581404a66faa3ef7
renypayus <khaireny@gmail.com>
2023-10-18 22:20:43 +0700

    Internet identity integration

--- src/internet_computer_frontend/src/index.js
+++ src/internet_computer_frontend/src/index.js
@@ -1,19 +1,38 @@
-import { internet_computer_backend } from "../../declarations/internet_computer_backend";
-
-document.querySelector("form").addEventListener("submit", async (e) => {
+import {
+  createActor,
+  internet_computer_backend,
+} from "../../declarations/internet_computer_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
+let actor = internet_computer_backend;
+console.log(process.env.CANISTER_ID_INTERNET_IDENTITY);
+const whoAmIButton = document.getElementById("whoAmI");
+whoAmIButton.onclick = async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await internet_computer_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
+  whoAmIButton.setAttribute("disabled", true);
+  const principal = await actor.whoami();
+  whoAmIButton.removeAttribute("disabled");
+  document.getElementById("principal").innerText = principal.toString();
   return false;
-});
+};
+const loginButton = document.getElementById("login");
+loginButton.onclick = async (e) => {
+  e.preventDefault();
+  let authClient = await AuthClient.create();
+  // start the login process and wait for it to finish
+  await new Promise((resolve) => {
+      authClient.login({
+          identityProvider:
+              process.env.DFX_NETWORK === "ic"
+                  ? "https://identity.ic0.app"
+                  : `http://localhost:4943/?canisterId=rdmx6-jaaaa-aaaaa-aaadq-cai`,
+          onSuccess: resolve,
+      });
+  });
+  const identity = authClient.getIdentity();
+  const agent = new HttpAgent({ identity });
+  actor = createActor(process.env.CANISTER_ID_internet_computer_BACKEND, {
+      agent,
+  });
+  return false;
+};
