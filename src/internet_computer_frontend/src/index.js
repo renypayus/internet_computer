f8206f5c34584d89878abbd7d35dc3d4c2318415
Khaireny <ren@Khairenys-MacBook-Pro.local>
2023-10-18 22:04:06 +0700

    Initial commit.

---
+++ src/internet_computer_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { internet_computer_backend } from "../../declarations/internet_computer_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await internet_computer_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
